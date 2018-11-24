6fa6f997a65690224fa1654da90c913c660bee64
Camillou22 <lambert.camille@gmail.com>
2018-11-24 13:36:57 +0100

    add debug mode again

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^u)oqwphm(f4iub&7&$n&k*oy6$6!grqer17_scyf7+rcb)+03'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
